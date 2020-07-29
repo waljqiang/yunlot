27bd2853fb5fe30ce3bf0b1408d7de8cedbe5f2c
waljqiang <waljqiang@163.com>
2020-07-29 18:22:24 +0800

    11

--- example/test.php
+++ example/test.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . "/../vendor/autoload.php";
 use Waljqiang\Yunlot\Yunlot;
 try{
-	$token = "yuncore";
+	$token = "cloudnetlot";
 	$type = "2";
 	$key = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFG";
 	$nonce = "abcdef";
